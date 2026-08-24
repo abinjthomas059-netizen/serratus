2ecf85cbc8b9574486dbc2aae590c4108356980d
Abin J Thomas <abinjthomas059@gmail.com>
2026-08-24 23:10:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -162,7 +162,6 @@ const NAV_LINKS = [
 const FLAGS = [
   { Flag: FlagIN, n: "India" }, { Flag: FlagUS, n: "USA" }, { Flag: FlagGB, n: "UK" },
   { Flag: FlagCA, n: "Canada" }, { Flag: FlagAU, n: "Australia" }, { Flag: FlagAE, n: "UAE" },
-  { Flag: FlagNZ, n: "New Zealand" },
   
 ];
 
